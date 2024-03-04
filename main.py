878e600da56713bce5431e99dd49f84b22bf54c1
anton <@hjghilukgj>
2024-03-04 07:31:10 +0300

    аа

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import math
 from pprint import pprint
 import time
 
-import numpy as np
-import matplotlib.pyplot as plt
+
+
 
 m0 = 4 * math.pi * 10 ** -7
 
@@ -86,7 +86,7 @@ def energy_matrix(x0=-9.5 * 10 ** -2, y0=0, r=2.4 * 10 ** -2, m=0.63, count_of_m
             En_matrix[alpha].append(Energy)
     return En_matrix
 
-def energy_matrix_with_real(x0=-9.5 * 10 ** -2, y0=0, r=2.4 * 10 ** -2, m=0.63, count_of_magnets=3, x_resolution=2, y_resolution=2, x_size=0.7*10**-2, y_size=2.3*10**-2):
+def energy_matrix_with_real(x0=-10 * 10 ** -2, y0=0, r=2.7 * 10 ** -2, m=0.63, count_of_magnets=3, x_resolution=6, y_resolution=6, x_size=0.7*10**-2, y_size=2.3*10**-2):
 
     m = m / (x_resolution * y_resolution)
 
@@ -94,7 +94,7 @@ def energy_matrix_with_real(x0=-9.5 * 10 ** -2, y0=0, r=2.4 * 10 ** -2, m=0.63, 
     list_of_main_coords = []
     list_of_second_coords = []
     magnet_moments = []
-    for i in range(360):
+    for i in range(120):
         curr_angle = i * math.pi / 180
         coords_x_main = []
         coords_y_main = []
@@ -119,38 +119,41 @@ def energy_matrix_with_real(x0=-9.5 * 10 ** -2, y0=0, r=2.4 * 10 ** -2, m=0.63, 
                 magnet_vector_x[j].append([])
                 magnet_vector_y[j].append([])
 
-                for y_deps in range(y_resolution ):
+                for y_deps in range(y_resolution):
                     x_m = (x_deps + x_deps + 1 - x_resolution) * (x_size / (2 * x_resolution))
                     y_m = (y_deps + y_deps + 1 - y_resolution) * (y_size / (2 * y_resolution))
+                    alpha = 0.000000001 + curr_angle + j * delta_angle
+                    if int(alpha * 180 / math.pi) % 90 != 0 or (int(alpha * 180 / math.pi) // 90) % 2 == 0:
+                        beta = math.atan(y_m / x_m)
+                        x0_ = r * math.cos(curr_angle + j * delta_angle)
+                        y0_ = r * math.sin(curr_angle + j * delta_angle)
+                        if alpha != 0:
+                            talpha = math.tan(alpha)
+                        else:
+                            talpha = math.tan(alpha)
 
-                    alpha = 0.000000000000001 + curr_angle + j * delta_angle
-                    beta = math.atan(y_m / x_m)
-                    x0_ = r * math.cos(curr_angle + j * delta_angle)
-                    y0_ = r * math.sin(curr_angle + j * delta_angle)
 
-                    if alpha != 0:
-                        talpha = math.tan(alpha)
+                        tfi = math.tan(beta - math.pi / 2 + alpha)
+                        if math.sin(alpha) != 0:
+                            x1 = x_m / math.sin(alpha)
+                        else:
+                            x1 = 0
+
+                        yc = talpha * x0_ - tfi * x0_
+
+                        flag = (alpha * 180 / math.pi) // 90
+                        # if flag == 1 or flag == 2 or flag == 5 or flag == 6 or flag == 9 or flag == 10:
+                        #     x2 = (yc + talpha * x1) / (talpha - tfi)
+                        #     y2 = talpha * (x2 - x1)
+                        #     x2 = (yc + talpha * x1) / (talpha - tfi)
+                        # else:
+                        x2 = (yc + talpha * x1) / (talpha - tfi)
+                        y2 = talpha * (x2 - x1)
                     else:
-                        talpha = math.tan(alpha)
+                        x2 = r * math.cos(curr_angle + j * delta_angle) + x_m
+                        y2 = r * math.sin(curr_angle + j * delta_angle) + y_m
 
-                    tfi = math.tan(beta - math.pi / 2 + alpha)
-                    if math.sin(alpha) != 0:
-                        x1 = x_m / math.sin(alpha)
-                    else:
-                        x1 = 0
-
-                    yc = talpha * x0_ - tfi * x0_
-
-                    flag = (alpha * 180 / math.pi) // 90
-                    # if flag == 1 or flag == 2 or flag == 5 or flag == 6 or flag == 9 or flag == 10:
-                    #     x2 = (yc + talpha * x1) / (talpha - tfi)
-                    #     y2 = talpha * (x2 - x1)
-                    #     x2 = (yc + talpha * x1) / (talpha - tfi)
-                    # else:
-                    x2 = (yc + talpha * x1) / (talpha - tfi)
-                    y2 = talpha * (x2 - x1)
-
-
+                    #print(x2, y2, alpha * 180 / math.pi, sep=';')
                     coords_x_main[j][x_deps].append(x2)
                     coords_y_main[j][x_deps].append(y2)
                     coords_x_second[j][x_deps].append(x0 + x2)
@@ -158,16 +161,16 @@ def energy_matrix_with_real(x0=-9.5 * 10 ** -2, y0=0, r=2.4 * 10 ** -2, m=0.63, 
 
                     magnet_vector_x[j][x_deps].append(m * math.cos(curr_angle + j * delta_angle))
                     magnet_vector_y[j][x_deps].append(m * math.sin(curr_angle + j * delta_angle))
-
         list_of_main_coords.append([coords_x_main, coords_y_main])
         list_of_second_coords.append([coords_x_second, coords_y_second])
         magnet_moments.append([magnet_vector_x, magnet_vector_y])
     En_matrix = []
-    for alpha in range(360):
+    for alpha in range(120):
         En_matrix.append([])
-        for beta in range(360):
+        for beta in range(120):
             Energy = 0
             for i in range(count_of_magnets):
+                
                 for j in range(count_of_magnets):
                     for x_deps_alpha in range(x_resolution):
                         for y_deps_alpha in range(y_resolution):
@@ -186,7 +189,10 @@ def energy_matrix_with_real(x0=-9.5 * 10 ** -2, y0=0, r=2.4 * 10 ** -2, m=0.63, 
                                                 list_of_second_coords[beta][1][i][x_deps_beta][y_deps_beta] - list_of_main_coords[alpha][1][j][x_deps_alpha][y_deps_alpha]) / r ** 5 -
                                                                  magnet_moments[alpha][1][j][x_deps_alpha][y_deps_alpha] / r ** 3)
                                     Energy += -(Bx * magnet_moments[beta][0][i][x_deps_beta][y_deps_beta] + By * magnet_moments[beta][1][i][x_deps_beta][y_deps_beta])
+
             En_matrix[alpha].append(Energy)
+        if alpha % 5 == 0:
+            print(alpha)
     return En_matrix
 
 
@@ -205,22 +211,22 @@ def interpolate_and_differentiate(matrix, a, b):
 
 
 
-
-e = energy_matrix()
-r = e
+start_time = time.time()
+e = energy_matrix_with_real()
+for i in range(len(e)):
+    e[i] = e[i] * 3
+e = e * 3
 # for i in range(3):
 #     for j in range(len(e)):
 #         r.append(e[j] + e[j] + e[j])
 
 
-
-a = open('e111.csv', 'w')
-for x in r:
+a = open('8points_res.csv', 'w')
+for x in e:
     a.write(';'.join(map(str, x)) + '\n')
-a.close()
+
+print("--- %s seconds ---" % (time.time() - start_time))
 exit()
-
-
 
 
 
